8fad9c21732edbc29b9c7aba6216f99f58dbbb7a
An KangMin <hkkm612@gmail.com>
2020-03-31 16:05:01 +0900

    Socket.io practice code for Chatting APP

---
+++ index.js
@@ -0,0 +1,33 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  //   res.send("<h1>Hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.broadcast.emit("broadcast_intro", {
+    introMsg: "Hello, this is SocketPrac"
+  });
+
+  socket.on("broadcast_intro", function(data) {
+    console.log("Broadcast Message : ", data.introMsg);
+    io.emit("boradcast_intro", data.introMsg);
+  });
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
